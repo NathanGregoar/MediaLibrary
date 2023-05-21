01727d42187464fa0a0acf72897ca8df7ef24603
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-05-21 18:04:01 +0200

    Update init.sql

--- MovieCollection/db/init.sql
+++ MovieCollection/db/init.sql
@@ -5,8 +5,8 @@ USE movie_collection;
 CREATE TABLE IF NOT EXISTS films (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
-  director VARCHAR(255) NOT NULL,
-  release_year INT NOT NULL
+  director VARCHAR(255) DEFAULT '/',
+  release_year INT DEFAULT 0
 );
 
 GRANT ALL PRIVILEGES ON movie_collection.* TO 'nathan'@'%' IDENTIFIED BY '444719';
