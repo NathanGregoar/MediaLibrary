8f338547d34aea2514f418dbe7b9372ba76c0bed
NathanGregoar <he201832@students.ephec.be>
2023-05-24 22:09:48 +0200

    Update admin.php

--- MovieCollection/web/admin/admin.php
+++ MovieCollection/web/admin/admin.php
@@ -1,147 +1,200 @@
-<?php
-// Connexion à la base de données (remplacez les valeurs par les vôtres)
-$host = 'db';
-$user = 'nathan';
-$password = '444719';
-$database = 'movie_collection';
-
-$connection = new mysqli($host, $user, $password, $database);
-
-// Vérifier la connexion
-if ($conn->connect_error) {
-    die("Connexion échouée: " . $conn->connect_error);
-}
-
-// Traitement du formulaire d'ajout de table
-if (isset($_POST['ajouter_table'])) {
-    $table_name = $_POST['nom_table'];
-
-    // Votre code pour ajouter la table à la base de données
-    $sql = "CREATE TABLE $table_name (
-                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                nom VARCHAR(30) NOT NULL,
-                email VARCHAR(50) NOT NULL
-            )";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Table $table_name ajoutée avec succès.";
-    } else {
-        echo "Erreur lors de la création de la table : " . $conn->error;
-    }
-}
-
-// Traitement du formulaire de suppression de table
-if (isset($_POST['supprimer_table'])) {
-    $table_name = $_POST['nom_table'];
-
-    // Votre code pour supprimer la table de la base de données
-    $sql = "DROP TABLE $table_name";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Table $table_name supprimée avec succès.";
-    } else {
-        echo "Erreur lors de la suppression de la table : " . $conn->error;
-    }
-}
-
-// Traitement du formulaire d'ajout d'élément
-if (isset($_POST['ajouter_element'])) {
-    $table_name = $_POST['nom_table'];
-    $nom = $_POST['nom'];
-    $email = $_POST['email'];
-
-    // Votre code pour ajouter l'élément à la table
-    $sql = "INSERT INTO $table_name (nom, email) VALUES ('$nom', '$email')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Élément ajouté avec succès.";
-    } else {
-        echo "Erreur lors de l'ajout de l'élément : " . $conn->error;
-    }
-}
-
-// Traitement du formulaire de modification d'élément
-if (isset($_POST['modifier_element'])) {
-    $table_name = $_POST['nom_table'];
-    $id = $_POST['id'];
-    $nom = $_POST['nom'];
-    $email = $_POST['email'];
-
-    // Votre code pour modifier l'élément dans la table
-    $sql = "UPDATE $table_name SET nom='$nom', email='$email' WHERE id=$id";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Élément modifié avec succès.";
-    } else {
-        echo "Erreur lors de la modification de l'élément : " . $conn->error;
-    }
-}
-
-// Traitement du formulaire de suppression d'élément
-if (isset($_POST['supprimer_element'])) {
-    $table_name = $_POST['nom_table'];
-    $id = $_POST['id'];
-
-    // Votre code pour supprimer l'élément de la table
-    $sql = "DELETE FROM $table_name WHERE id=$id";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "Élément supprimé avec succès.";
-    } else {
-        echo "Erreur lors de la suppression de l'élément : " . $conn->error;
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Gestion de la base de données</title>
+    <title>Admin</title>
 </head>
 <body>
-    <div class="navbar">
-        <a href="../index.php">Accueil</a>
-    </div>
+    <?php
+    // Configuration de la base de données
+    $servername = "db";
+    $username = "nathan";
+    $password = "444719";
+    $database = "movie_collection";
 
+    // Connexion à la base de données
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Vérification de la connexion
+    if ($conn->connect_error) {
+        die("Erreur de connexion à la base de données: " . $conn->connect_error);
+    }
+
+    // Vérification des actions à effectuer
+    if (isset($_POST['action'])) {
+        $action = $_POST['action'];
+
+        switch ($action) {
+            case 'ajouter_table':
+                $table_name = $_POST['table_name'];
+
+                // Création de la table
+                $sql = "CREATE TABLE $table_name (
+                    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    nom VARCHAR(30) NOT NULL,
+                    email VARCHAR(50),
+                    telephone VARCHAR(15)
+                )";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "La table $table_name a été ajoutée avec succès.";
+                } else {
+                    echo "Erreur lors de la création de la table: " . $conn->error;
+                }
+                break;
+
+            case 'supprimer_table':
+                $table_name = $_POST['table_name'];
+
+                // Suppression de la table
+                $sql = "DROP TABLE $table_name";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "La table $table_name a été supprimée avec succès.";
+                } else {
+                    echo "Erreur lors de la suppression de la table: " . $conn->error;
+                }
+                break;
+
+            case 'ajouter_element':
+                $table_name = $_POST['table_name'];
+                $champ_name = $_POST['champ_name'];
+                $valeur = $_POST['valeur'];
+
+                // Ajout de l'élément dans la table
+                $sql = "INSERT INTO $table_name ($champ_name) VALUES ('$valeur')";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "L'élément a été ajouté avec succès dans la table $table_name.";
+                } else {
+                    echo "Erreur lors de l'ajout de l'élément: " . $conn->error;
+                }
+                break;
+
+            case 'modifier_element':
+                $table_name = $_POST['table_name'];
+                $id = $_POST['id'];
+                $champ_name = $_POST['champ_name'];
+                $valeur = $_POST['valeur'];
+
+                // Mise à jour de l'élément dans la table
+                $sql = "UPDATE $table_name SET $champ_name = '$valeur' WHERE id = $id";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "L'élément a été modifié avec succès dans la table $table_name.";
+                } else {
+                    echo "Erreur lors de la modification de l'élément: " . $conn->error;
+                }
+                break;
+
+            case 'supprimer_element':
+                $table_name = $_POST['table_name'];
+                $id = $_POST['id'];
+
+                // Suppression de l'élément de la table
+                $sql = "DELETE FROM $table_name WHERE id = $id";
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "L'élément a été supprimé avec succès de la table $table_name.";
+                } else {
+                    echo "Erreur lors de la suppression de l'élément: " . $conn->error;
+                }
+                break;
+
+            default:
+                echo "Action non valide.";
+                break;
+        }
+    }
+    ?>
+
+    <!-- Formulaire d'ajout de table -->
     <h2>Ajouter une table</h2>
-    <form method="post">
-        <input type="text" name="nom_table" placeholder="Nom de la table" required>
-        <input type="submit" name="ajouter_table" value="Ajouter">
+    <form method="post" action="">
+        <input type="hidden" name="action" value="ajouter_table">
+        <label for="table_name">Nom de la table:</label>
+        <input type="text" name="table_name" required>
+        <input type="submit" value="Ajouter">
     </form>
 
+    <!-- Formulaire de suppression de table -->
     <h2>Supprimer une table</h2>
-    <form method="post">
-        <input type="text" name="nom_table" placeholder="Nom de la table" required>
-        <input type="submit" name="supprimer_table" value="Supprimer">
+    <form method="post" action="">
+        <input type="hidden" name="action" value="supprimer_table">
+        <label for="table_name">Nom de la table:</label>
+        <input type="text" name="table_name" required>
+        <input type="submit" value="Supprimer">
     </form>
 
+    <!-- Formulaire d'ajout d'élément -->
     <h2>Ajouter un élément</h2>
-    <form method="post">
-        <input type="text" name="nom_table" placeholder="Nom de la table" required>
-        <input type="text" name="nom" placeholder="Nom" required>
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="submit" name="ajouter_element" value="Ajouter">
+    <form method="post" action="">
+        <input type="hidden" name="action" value="ajouter_element">
+        <label for="table_name">Table:</label>
+        <input type="text" name="table_name" required>
+        <label for="champ_name">Champ:</label>
+        <input type="text" name="champ_name" required>
+        <label for="valeur">Valeur:</label>
+        <input type="text" name="valeur" required>
+        <input type="submit" value="Ajouter">
     </form>
 
+    <!-- Formulaire de modification d'élément -->
     <h2>Modifier un élément</h2>
-    <form method="post">
-        <input type="text" name="nom_table" placeholder="Nom de la table" required>
-        <input type="number" name="id" placeholder="ID de l'élément" required>
-        <input type="text" name="nom" placeholder="Nom" required>
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="submit" name="modifier_element" value="Modifier">
+    <form method="post" action="">
+        <input type="hidden" name="action" value="modifier_element">
+        <label for="table_name">Table:</label>
+        <input type="text" name="table_name" required>
+        <label for="id">ID:</label>
+        <input type="text" name="id" required>
+        <label for="champ_name">Champ:</label>
+        <input type="text" name="champ_name" required>
+        <label for="valeur">Nouvelle valeur:</label>
+        <input type="text" name="valeur" required>
+        <input type="submit" value="Modifier">
     </form>
 
+    <!-- Formulaire de suppression d'élément -->
     <h2>Supprimer un élément</h2>
-    <form method="post">
-        <input type="text" name="nom_table" placeholder="Nom de la table" required>
-        <input type="number" name="id" placeholder="ID de l'élément" required>
-        <input type="submit" name="supprimer_element" value="Supprimer">
+    <form method="post" action="">
+        <input type="hidden" name="action" value="supprimer_element">
+        <label for="table_name">Table:</label>
+        <input type="text" name="table_name" required>
+        <label for="id">ID:</label>
+        <input type="text" name="id" required>
+        <input type="submit" value="Supprimer">
     </form>
+
+    <!-- Affichage de la structure de la base de données -->
+    <h2>Structure de la base de données</h2>
+    <?php
+    $sql = "SHOW TABLES";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $table = $row['Tables_in_' . $database];
+            echo "<h3>$table</h3>";
+            echo "<ul>";
+
+            $sql_columns = "SHOW COLUMNS FROM $table";
+            $result_columns = $conn->query($sql_columns);
+
+            if ($result_columns->num_rows > 0) {
+                while ($row_columns = $result_columns->fetch_assoc()) {
+                    echo "<li>" . $row_columns['Field'] . " (" . $row_columns['Type'] . ")</li>";
+                }
+            } else {
+                echo "<li>Aucune colonne trouvée.</li>";
+            }
+
+            echo "</ul>";
+        }
+    } else {
+        echo "Aucune table trouvée dans la base de données.";
+    }
+
+    // Fermeture de la connexion à la base de données
+    $conn->close();
+    ?>
 </body>
 </html>
-
-<?php
-// Fermer la connexion à la base de données
-$conn->close();
-?>
