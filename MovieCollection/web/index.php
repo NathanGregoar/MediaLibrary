19808120ad80ed78dc108155395b83a2d222b36e
NathanGregoar <he201832@students.ephec.be>
2023-05-24 18:00:39 +0200

    Modif structure

--- MovieCollection/web/index.php
+++ MovieCollection/web/index.php
@@ -6,15 +6,9 @@
 </head>
 <body>
     <div class="container">
-        <h1>Bienvenue sur Movie Collection</h1>
-
-        <div class="intro-section">
-            <h2>Explorez notre collection de films</h2>
-            <p>Découvrez de nouvelles aventures cinématographiques.</p>
-        </div>
-
         <div class="section-links">
             <a href="film/film.php" class="section-link">Section Films</a>
+            <a href="serie/serie.php" class="section-link">Section Séries</a>
             <!-- Ajoutez ici d'autres liens vers vos différentes sections -->
         </div>
     </div>
