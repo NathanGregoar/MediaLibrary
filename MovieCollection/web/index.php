3534e8314d860af3d539eecdcebc1654c92e2780
NathanGregoar <he201832@students.ephec.be>
2023-05-24 17:53:46 +0200

    Modif accueil + css

--- MovieCollection/web/index.php
+++ MovieCollection/web/index.php
@@ -8,8 +8,13 @@
     <div class="container">
         <h1>Bienvenue sur Movie Collection</h1>
 
+        <div class="intro-section">
+            <h2>Explorez notre collection de films</h2>
+            <p>Découvrez de nouvelles aventures cinématographiques.</p>
+        </div>
+
         <div class="section-links">
-            <a href="film/film.php" class="section-link">Aller à la section Films</a>
+            <a href="film/film.php" class="section-link">Section Films</a>
             <!-- Ajoutez ici d'autres liens vers vos différentes sections -->
         </div>
     </div>
