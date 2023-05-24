9870d7f82478b97253073c79ba23aaa983422d11
NathanGregoar <he201832@students.ephec.be>
2023-05-24 21:09:36 +0200

    Modif accueil

--- MovieCollection/web/index.php
+++ MovieCollection/web/index.php
@@ -5,26 +5,26 @@
     <link rel="stylesheet" type="text/css" href="accueil.css">
 </head>
 <body>
-    <div class="container">
-        <div class="admin-section">
-            <?php
-            if (isset($_POST['code'])) {
-                $code = $_POST['code'];
-                if ($code == '444719') {
-                    header('Location: admin/admin.php');
-                    exit();
-                } else {
-                    echo '<div class="alert alert-error">Le code est incorrect. Veuillez réessayer.</div>';
-                }
+    <div class="admin-section">
+        <?php
+        if (isset($_POST['code'])) {
+            $code = $_POST['code'];
+            if ($code == '444719') {
+                header('Location: admin/admin.php');
+                exit();
+            } else {
+                echo '<div class="alert alert-error">Le code est incorrect. Veuillez réessayer.</div>';
             }
-            ?>
-            <form method="POST" class="admin-form">
-                <label for="code">Code d'accès :</label>
-                <input type="password" id="code" name="code" required>
-                <input type="submit" value="Se connecter">
-            </form>
-        </div>
+        }
+        ?>
+        <form method="POST" class="admin-form">
+            <label for="code">Code d'accès :</label>
+            <input type="password" id="code" name="code" required>
+            <input type="submit" value="Se connecter">
+        </form>
+    </div>
 
+    <div class="container">
         <div class="section-links">
             <a href="film/film.php" class="section-link">
                 <span class="section-link-text">Section Films</span>
