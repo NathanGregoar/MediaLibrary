3ee9478622e853394cb08e42480045102b515de6
NathanGregoar <he201832@students.ephec.be>
2023-05-24 20:49:05 +0200

    Update index.php

--- MovieCollection/web/index.php
+++ MovieCollection/web/index.php
@@ -8,9 +8,11 @@
     <div class="container">
         <div class="section-links">
             <a href="film/film.php" class="section-link">
+                <img src="https://png.pngtree.com/png-vector/20190816/ourlarge/pngtree-film-logo-design-template-vector-isolated-illustration-png-image_1693431.jpg" alt="Film Logo" class="section-link-image">
                 <span class="section-link-text">Section Films</span>
             </a>
             <a href="serie/serie.php" class="section-link">
+                <img src="https://png.pngtree.com/png-vector/20190816/ourlarge/pngtree-film-logo-design-template-vector-isolated-illustration-png-image_1693431.jpg" alt="Film Logo" class="section-link-image">
                 <span class="section-link-text">Section Séries</span>
             </a>
             <!-- Ajoutez ici d'autres liens vers vos différentes sections -->
