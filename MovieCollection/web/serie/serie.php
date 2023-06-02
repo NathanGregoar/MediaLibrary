465fbbead134fd371383bcb5040889cd3a81b8ff
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-06-02 15:39:36 +0200

    Ajouts des pages series

--- MovieCollection/web/serie/serie.php
+++ MovieCollection/web/serie/serie.php
@@ -8,8 +8,80 @@
     <div class="container">
         <div class="navbar">
             <a href="../index.php">Accueil</a>
-            <a href="serie.php">Ajouter une Série</a>
+            <a href="serie.php">Ajouter une Serie</a>
+            <a href="search_serie.php">Consulter les Séries</a>
         </div>
+
+        <h1>Ajouter une Série</h1>
+
+        <?php
+        $host = 'db';
+        $user = 'nathan';
+        $password = '444719';
+        $database = 'movie_collection';
+
+        $connection = new mysqli($host, $user, $password, $database);
+
+        if ($connection->connect_error) {
+            die('Erreur de connexion : ' . $connection->connect_error);
+        }
+
+        // Ajout d'une série
+        if (isset($_POST['title'])) {
+            $title = $connection->real_escape_string($_POST['title']);
+            $langueSerie = $_POST['langue_serie'] != '' ? "'" . $connection->real_escape_string($_POST['langue_serie']) . "'" : 'NULL';
+            $completeSeason = isset($_POST['complete_season']) ? 1 : 0;
+            $episodeCount = $_POST['episode_count'] != '' ? $connection->real_escape_string($_POST['episode_count']) : 'NULL';
+            $seasonNumber = $_POST['season_number'] != '' ? $connection->real_escape_string($_POST['season_number']) : 'NULL';
+            $externalHardDrive = $_POST['external_hard_drive'] != '' ? "'" . $connection->real_escape_string($_POST['external_hard_drive']) . "'" : 'NULL';
+
+            // Vérifier les doublons
+            $duplicateSql = "SELECT * FROM series WHERE title = '$title' AND langue_serie = $langueSerie AND season_number = $seasonNumber";
+            $duplicateResult = $connection->query($duplicateSql);
+
+            if ($duplicateResult->num_rows > 0) {
+                echo '<div class="alert alert-error">La série existe déjà dans la base de données.</div>';
+            } else {
+                $insertSql = "INSERT INTO series (title, langue_serie, complete_season, episode_count, season_number, external_hard_drive) VALUES ('$title', $langueSerie, $completeSeason, $episodeCount, $seasonNumber, $externalHardDrive)";
+
+                if ($connection->query($insertSql) === TRUE) {
+                    echo '<div class="alert alert-success">Série ajoutée avec succès !</div>';
+                } else {
+                    echo '<div class="alert alert-error">Erreur lors de l\'ajout de la série : ' . $connection->error . '</div>';
+                }
+            }
+        }
+        ?>
+
+        <form method="POST">
+            <label for="title">Titre :</label>
+            <input type="text" id="title" name="title" required>
+
+            <label for="langue_serie">Langue de la série :</label>
+            <select id="langue_serie" name="langue_serie">
+                <option value="Français">Français</option>
+                <option value="Anglais">Anglais</option>
+                <option value="Coréen">Coréen</option>
+            </select>
+
+            <label for="complete_season">Saison complète :</label>
+            <input type="checkbox" id="complete_season" name="complete_season">
+
+            <label for="episode_count">Nombre d'épisodes :</label>
+            <input type="text" id="episode_count" name="episode_count">
+
+            <label for="season_number">Numéro de saison :</label>
+            <input type="text" id="season_number" name="season_number">
+
+            <label for="external_hard_drive">Numéro du disque dur externe :</label>
+            <input type="text" id="external_hard_drive" name="external_hard_drive">
+
+            <input type="submit" value="Ajouter">
+        </form>
+
+        <?php
+        $connection->close();
+        ?>
     </div>
 </body>
 </html>
