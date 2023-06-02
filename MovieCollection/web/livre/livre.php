e319be4f99d4a23082a7cf56cf0e54be82b4fb53
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-06-02 16:43:38 +0200

    Update livre.php

--- MovieCollection/web/livre/livre.php
+++ MovieCollection/web/livre/livre.php
@@ -3,6 +3,37 @@
 <head>
     <title>Ajouter un Livre</title>
     <link rel="stylesheet" type="text/css" href="livre.css">
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #ecf2f9;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        h1 {
+            text-align: center;
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        p {
+            text-align: center;
+            font-size: 18px;
+            color: #555;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
@@ -10,6 +41,8 @@
             <a href="../index.php">Accueil</a>
             <a href="livre.php">Ajouter un Livre</a>
         </div>
+        <h1>Cette page n'est pas encore terminée.</h1>
+        <p>Elle sera bientôt disponible.</p>
     </div>
 </body>
 </html>
