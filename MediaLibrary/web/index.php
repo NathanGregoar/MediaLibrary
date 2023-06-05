1427fa319fb3d0bb9554c0bde0d4e6627e2067ef
NathanGregoar <he201832@students.ephec.be>
2023-06-06 00:47:34 +0200

    fichier auth.php

--- MediaLibrary/web/index.php
+++ MediaLibrary/web/index.php
@@ -1,18 +1,5 @@
 <?php
-session_start();
-
-// Fonction de vérification si l'utilisateur est connecté
-function checkLoggedIn() {
-    if (!isset($_SESSION['username'])) {
-        header("Location: login.php");
-        exit();
-    }
-}
-
-// Vérification si l'utilisateur est connecté
-checkLoggedIn();
-
-$username = $_SESSION['username'];
+require_once 'auth.php';
 
 // Traitement de la déconnexion
 if (isset($_POST['logout'])) {
