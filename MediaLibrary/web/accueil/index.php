05bb0d670f4b8f9753aae3299105e4b02b55a535
NathanGregoar <he201832@students.ephec.be>
2023-06-06 23:10:47 +0200

    Modif accueil

--- MediaLibrary/web/accueil/index.php
+++ MediaLibrary/web/accueil/index.php
@@ -33,26 +33,28 @@ $username = $_SESSION['username'];
 
     <div class="container">
         <h1 class="welcome-message">Bienvenue, <?php echo $username; ?> !</h1>
-        <div class="section-links">
-            <a href="film/film.php" class="section-link">
-                <span class="section-link-text">Section Films</span>
-            </a>
-            <a href="serie/serie.php" class="section-link">
-                <span class="section-link-text">Section Séries / Drama</span>
-            </a>
-            <a href="livre/livre.php" class="section-link">
-                <span class="section-link-text">Section Livres</span>
-            </a>
-            <a href="concert/concert.php" class="section-link">
-                <span class="section-link-text">Section Concerts</span>
-            </a>
-            <a href="documentaire/documentaire.php" class="section-link">
-                <span class="section-link-text">Section Documentaires</span>
-            </a>
-            <!-- Ajoutez ici d'autres liens vers vos différentes sections -->
-        </div>
+    </div>
+
+    <div class="section-links">
+        <a href="film/film.php" class="section-link">
+            <span class="section-link-text">Section Films</span>
+        </a>
+        <a href="serie/serie.php" class="section-link">
+            <span class="section-link-text">Section Séries / Drama</span>
+        </a>
+        <a href="livre/livre.php" class="section-link">
+            <span class="section-link-text">Section Livres</span>
+        </a>
+        <a href="concert/concert.php" class="section-link">
+            <span class="section-link-text">Section Concerts</span>
+        </a>
+        <a href="documentaire/documentaire.php" class="section-link">
+            <span class="section-link-text">Section Documentaires</span>
+        </a>
+        <!-- Ajoutez ici d'autres liens vers vos différentes sections -->
     </div>
 </body>
 </html>
+
 
 
