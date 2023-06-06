952bec2e0b87ccbb3856e7f39ef09626cdb81ce9
NathanGregoar <he201832@students.ephec.be>
2023-06-06 22:36:10 +0200

    Update index.php

--- MediaLibrary/web/accueil/index.php
+++ MediaLibrary/web/accueil/index.php
@@ -11,6 +11,11 @@ if (isset($_POST['logout'])) {
     header("Location: ../login.php");
     exit();
 }
+
+// Vérification si l'utilisateur est connecté
+checkLoggedIn();
+
+$username = $_SESSION['username'];
 ?>
 
 <!DOCTYPE html>
@@ -23,40 +28,33 @@ if (isset($_POST['logout'])) {
     <header>
         <div class="container">
             <h1 class="title">MediaLibrary</h1>
-            <?php if (isLoggedIn()): ?>
-                <form method="post" action="" class="logout-form">
-                    <input type="submit" name="logout" value="Déconnexion">
-                </form>
-            <?php endif; ?>
+            <form method="post" action="" class="logout-form">
+                <input type="submit" name="logout" value="Déconnexion">
+            </form>
         </div>
     </header>
 
     <div class="container">
-        <?php if (isLoggedIn()): ?>
-            <h2 class="welcome-message">Bienvenue, <?php echo $username; ?> !</h2>
-            <p class="description">Ceci est la page d'accueil.</p>
-            <div class="section-links">
-                <a href="film/film.php" class="section-link">
-                    <span class="section-link-text">Section Films</span>
-                </a>
-                <a href="serie/serie.php" class="section-link">
-                    <span class="section-link-text">Section Séries / Drama</span>
-                </a>
-                <a href="livre/livre.php" class="section-link">
-                    <span class="section-link-text">Section Livres</span>
-                </a>
-                <a href="concert/concert.php" class="section-link">
-                    <span class="section-link-text">Section Concerts</span>
-                </a>
-                <a href="documentaire/documentaire.php" class="section-link">
-                    <span class="section-link-text">Section Documentaires</span>
-                </a>
-                <!-- Ajoutez ici d'autres liens vers vos différentes sections -->
-            </div>
-        <?php else: ?>
-            <p class="login-message">Veuillez vous connecter pour accéder à l'accueil.</p>
-            <a href="../login.php" class="login-link">Se connecter</a>
-        <?php endif; ?>
+        <h2 class="welcome-message">Bienvenue, <?php echo $username; ?> !</h2>
+        <p class="description">Ceci est la page d'accueil.</p>
+        <div class="section-links">
+            <a href="film/film.php" class="section-link">
+                <span class="section-link-text">Section Films</span>
+            </a>
+            <a href="serie/serie.php" class="section-link">
+                <span class="section-link-text">Section Séries / Drama</span>
+            </a>
+            <a href="livre/livre.php" class="section-link">
+                <span class="section-link-text">Section Livres</span>
+            </a>
+            <a href="concert/concert.php" class="section-link">
+                <span class="section-link-text">Section Concerts</span>
+            </a>
+            <a href="documentaire/documentaire.php" class="section-link">
+                <span class="section-link-text">Section Documentaires</span>
+            </a>
+            <!-- Ajoutez ici d'autres liens vers vos différentes sections -->
+        </div>
     </div>
 </body>
 </html>
