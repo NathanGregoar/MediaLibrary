8c5ae482d8ab5bfa01983e70041bba30f27b2c7a
NathanGregoar <he201832@students.ephec.be>
2023-06-06 16:55:39 +0200

    ajout déconnexion

--- MediaLibrary/web/accueil/index.php
+++ MediaLibrary/web/accueil/index.php
@@ -13,7 +13,19 @@ function checkLoggedIn() {
 checkLoggedIn();
 
 $username = $_SESSION['username'];
+
+// Vérification si le bouton de déconnexion a été cliqué
+if (isset($_POST['logout'])) {
+    // Suppression de toutes les variables de session
+    session_unset();
+    // Destruction de la session
+    session_destroy();
+    // Redirection vers la page de connexion
+    header("Location: login.php");
+    exit();
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,6 +34,9 @@ $username = $_SESSION['username'];
 <body>
     <h1>Bienvenue, <?php echo $username; ?> !</h1>
     <p>Ceci est la page d'accueil.</p>
+    <form method="post" action="">
+        <input type="submit" name="logout" value="Déconnexion">
+    </form>
     <a href="../film/film.php">Aller à la page des films</a>
 </body>
 </html>
