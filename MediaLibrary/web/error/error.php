1296339ed8895bbe5961baac3e9c89f226970a1f
NathanGregoar <he201832@students.ephec.be>
2023-06-06 22:54:30 +0200

    ajout d'une page error

---
+++ MediaLibrary/web/error/error.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Erreur 404 - Page non trouvée</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+
+        h1 {
+            color: #ff0000;
+        }
+    </style>
+</head>
+<body>
+    <h1>Erreur 404 - Page non trouvée</h1>
+    <p>La page que vous recherchez n'existe pas.</p>
+    <!-- Vous pouvez personnaliser davantage votre page d'erreur ici -->
+</body>
+</html>
