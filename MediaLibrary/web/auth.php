8f3214dcb00623c6eae9c9eac5c507f917becf5b
NathanGregoar <he201832@students.ephec.be>
2023-06-06 17:06:21 +0200

    création auth.php

---
+++ MediaLibrary/web/auth.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+// Fonction de vérification si l'utilisateur est connecté
+function checkLoggedIn() {
+    if (!isset($_SESSION['username'])) {
+        header("Location: login.php");
+        exit();
+    }
+}
+
+// Vérification si l'utilisateur est connecté
+checkLoggedIn();
+
+$username = $_SESSION['username'];
+?>
