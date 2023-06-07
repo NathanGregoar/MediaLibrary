b8af95a6bed4114df0e10af483d3251430383037
NathanGregoar <he201832@students.ephec.be>
2023-06-07 17:17:08 +0200

    Update auth.php

--- MediaLibrary/web/utils/auth.php
+++ MediaLibrary/web/utils/auth.php
@@ -18,7 +18,38 @@ function getLoggedInUserId() {
     if (isset($_SESSION['user_id'])) {
         return $_SESSION['user_id'];
     } else {
-        return null;
+        // Effectuer la logique pour récupérer l'ID de l'utilisateur à partir de la base de données
+        $host = 'db';
+        $user = 'nathan';
+        $password = '444719';
+        $database = 'media_library';
+
+        $connection = new mysqli($host, $user, $password, $database);
+
+        if ($connection->connect_error) {
+            die('Erreur de connexion : ' . $connection->connect_error);
+        }
+
+        $loggedInUsername = $_SESSION['username'];
+
+        $query = "SELECT id FROM users WHERE username = ?";
+        $stmt = $connection->prepare($query);
+        $stmt->bind_param("s", $loggedInUsername);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $loggedInUserId = $row['id'];
+            $_SESSION['user_id'] = $loggedInUserId; // Stocker l'ID de l'utilisateur en session pour les prochaines requêtes
+            $stmt->close();
+            $connection->close();
+            return $loggedInUserId;
+        } else {
+            $stmt->close();
+            $connection->close();
+            return null;
+        }
     }
 }
 ?>
