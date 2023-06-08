cc60a3aa83d5aab5ee0851c76d3de0bc93de0434
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-06-08 20:12:02 +0200

    Update auth.php

--- MediaLibrary/web/utils/auth.php
+++ MediaLibrary/web/utils/auth.php
@@ -52,4 +52,45 @@ function getLoggedInUser() {
         }
     }
 }
+
+// Mettre à jour la fonction getLoggedInUserId() pour récupérer l'adresse e-mail de l'utilisateur
+function getLoggedInUserId() {
+    if (isset($_SESSION['user_id'])) {
+        return $_SESSION['user_id'];
+    } else {
+        // Effectuer la logique pour récupérer l'ID de l'utilisateur à partir de la base de données
+        $host = 'db';
+        $user = 'nathan';
+        $password = '444719';
+        $database = 'media_library';
+
+        $connection = new mysqli($host, $user, $password, $database);
+
+        if ($connection->connect_error) {
+            die('Erreur de connexion : ' . $connection->connect_error);
+        }
+
+        $loggedInUsername = $_SESSION['username'];
+
+        $query = "SELECT id, email FROM users WHERE username = ?";
+        $stmt = $connection->prepare($query);
+        $stmt->bind_param("s", $loggedInUsername);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $loggedInUserId = $row['id'];
+            $_SESSION['user_id'] = $loggedInUserId; // Stocker l'ID de l'utilisateur en session pour les prochaines requêtes
+            $_SESSION['email'] = $row['email']; // Stocker l'e-mail de l'utilisateur en session pour les prochaines requêtes
+            $stmt->close();
+            $connection->close();
+            return $loggedInUserId;
+        } else {
+            $stmt->close();
+            $connection->close();
+            return null;
+        }
+    }
+}
 ?>
