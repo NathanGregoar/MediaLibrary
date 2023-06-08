7446bb8ce37f79e4b6ca6ccf07d186e0298053c0
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-06-08 20:46:31 +0200

    Update admin.php

--- MediaLibrary/web/admin/admin.php
+++ MediaLibrary/web/admin/admin.php
@@ -1,6 +1,15 @@
 <?php
 require_once '../utils/auth.php';
-require_once '../utils/check_admin.php';
+
+$username = $_SESSION['username'];
+$email = $_SESSION['email'];
+
+// Vérification si l'utilisateur est autorisé à accéder à la page d'administration
+if ($username !== "Nathan" || $email !== "nathan.gregoar@yahoo.fr") {
+    // Redirection vers la page d'accueil
+    header("Location: ../accueil/index.php");
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
