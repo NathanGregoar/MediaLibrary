cfd0da976d2cd1b79da2d8f33b8bfdbdcfe47c98
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-06-08 20:16:18 +0200

    Update auth.php

--- MediaLibrary/web/utils/auth.php
+++ MediaLibrary/web/utils/auth.php
@@ -42,46 +42,6 @@ function getLoggedInUser() {
             $row = $result->fetch_assoc();
             $loggedInUser = $row;
             $_SESSION['user'] = $loggedInUser; // Stocker les informations de l'utilisateur en session pour les prochaines requêtes
-            $stmt->close();
-            $connection->close();
-            return $loggedInUser;
-        } else {
-            $stmt->close();
-            $connection->close();
-            return null;
-        }
-    }
-}
-
-// Mettre à jour la fonction getLoggedInUserId() pour récupérer l'adresse e-mail de l'utilisateur
-function getLoggedInUser() {
-    if (isset($_SESSION['user'])) {
-        return $_SESSION['user'];
-    } else {
-        // Effectuer la logique pour récupérer les informations de l'utilisateur à partir de la base de données
-        $host = 'db';
-        $user = 'nathan';
-        $password = '444719';
-        $database = 'media_library';
-
-        $connection = new mysqli($host, $user, $password, $database);
-
-        if ($connection->connect_error) {
-            die('Erreur de connexion : ' . $connection->connect_error);
-        }
-
-        $loggedInUsername = $_SESSION['username'];
-
-        $query = "SELECT * FROM users WHERE username = ?";
-        $stmt = $connection->prepare($query);
-        $stmt->bind_param("s", $loggedInUsername);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            $loggedInUser = $row;
-            $_SESSION['user'] = $loggedInUser; // Stocker les informations de l'utilisateur en session pour les prochaines requêtes
             $_SESSION['email'] = $row['email']; // Stocker l'e-mail de l'utilisateur en session pour les prochaines requêtes
             $stmt->close();
             $connection->close();
