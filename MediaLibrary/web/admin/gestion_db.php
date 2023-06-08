7b49d0e4e325800d2acc23b2148f509df49056ae
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-06-08 21:09:33 +0200

    Update gestion_db.php

--- MediaLibrary/web/admin/gestion_db.php
+++ MediaLibrary/web/admin/gestion_db.php
@@ -26,200 +26,192 @@ if ($username !== "Nathan" || $email !== "nathan.gregoar@yahoo.fr") {
     <div class="container">
         <div class="left-section">
         <?php
-        // Connexion à la base de données
-            $conn = new mysqli($servername, $username, $password, $database);
+        // Vérification des actions à effectuer
+        if (isset($_POST['action'])) {
+            $action = $_POST['action'];
 
-            // Vérification de la connexion
-            if ($conn->connect_error) {
-                die("Erreur de connexion à la base de données: " . $conn->connect_error);
-            }
+            switch ($action) {
+                case 'ajouter_table':
+                    $table_name = $_POST['table_name'];
 
-            // Vérification des actions à effectuer
-            if (isset($_POST['action'])) {
-                $action = $_POST['action'];
+                    // Création de la table
+                    $sql = "CREATE TABLE $table_name (
+                        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY
+                    )";
 
-                switch ($action) {
-                    case 'ajouter_table':
-                        $table_name = $_POST['table_name'];
-
-                        // Création de la table
-                        $sql = "CREATE TABLE $table_name (
-                            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY
-                        )";
-
-                        if ($conn->query($sql) === TRUE) {
-                            echo "La table $table_name a été ajoutée avec succès.";
-                        } else {
-                            echo "Erreur lors de la création de la table: " . $conn->error;
-                        }
-                        break;
-
-                    case 'supprimer_table':
-                        $table_name = $_POST['table_name'];
-
-                        // Suppression de la table
-                        $sql = "DROP TABLE $table_name";
-
-                        if ($conn->query($sql) === TRUE) {
-                            echo "La table $table_name a été supprimée avec succès.";
-                        } else {
-                            echo "Erreur lors de la suppression de la table: " . $conn->error;
-                        }
-                        break;
-
-                    case 'ajouter_colonne':
-                        $table_name = $_POST['table_name'];
-                        $colonne_name = $_POST['colonne_name'];
-                        $colonne_type = $_POST['colonne_type'];
-
-                        // Ajout de la colonne dans la table
-                        $sql = "ALTER TABLE $table_name ADD COLUMN $colonne_name $colonne_type";
-
-                        if ($conn->query($sql) === TRUE) {
-                            echo "La colonne $colonne_name a été ajoutée avec succès à la table $table_name.";
-                        } else {
-                            echo "Erreur lors de l'ajout de la colonne: " . $conn->error;
-                        }
-                        break;
-
-                    case 'supprimer_colonne':
-                        $table_name = $_POST['table_name'];
-                        $colonne_name = $_POST['colonne_name'];
-
-                        // Suppression de la colonne de la table
-                        $sql = "ALTER TABLE $table_name DROP COLUMN $colonne_name";
-
-                        if ($conn->query($sql) === TRUE) {
-                            echo "La colonne $colonne_name a été supprimée avec succès de la table $table_name.";
-                        } else {
-                            echo "Erreur lors de la suppression de la colonne: " . $conn->error;
-                        }
-                        break;
-
-                    case 'modifier_colonne':
-                        $table_name = $_POST['table_name'];
-                        $colonne_name = $_POST['colonne_name'];
-                        $nouveau_nom = $_POST['nouveau_nom'];
-                        $nouveau_type = $_POST['nouveau_type'];
-
-                        // Modification de la colonne dans la table
-                        $sql = "ALTER TABLE $table_name CHANGE COLUMN $colonne_name $nouveau_nom $nouveau_type";
-
-                        if ($conn->query($sql) === TRUE) {
-                            echo "La colonne $colonne_name a été modifiée avec succès dans la table $table_name.";
-                        } else {
-                            echo "Erreur lors de la modification de la colonne: " . $conn->error;
-                        }
-                        break;
-
-                    default:
-                        echo "Action non valide.";
-                        break;
-                }
-            }
-            ?>
-
-            <!-- Formulaire d'ajout de table -->
-            <h2>Ajouter une table</h2>
-            <form method="post" action="">
-                <input type="hidden" name="action" value="ajouter_table">
-                <label for="table_name">Nom de la table:</label>
-                <input type="text" name="table_name" required>
-                <input type="submit" value="Ajouter">
-            </form>
-
-            <!-- Formulaire de suppression de table -->
-            <h2>Supprimer une table</h2>
-            <form method="post" action="">
-                <input type="hidden" name="action" value="supprimer_table">
-                <label for="table_name">Nom de la table:</label>
-                <input type="text" name="table_name" required>
-                <input type="submit" value="Supprimer">
-            </form>
-
-            <!-- Formulaire d'ajout de colonne -->
-            <h2>Ajouter une colonne</h2>
-            <form method="post" action="">
-                <input type="hidden" name="action" value="ajouter_colonne">
-                <label for="table_name">Table:</label>
-                <input type="text" name="table_name" required>
-                <label for="colonne_name">Nom de la colonne:</label>
-                <input type="text" name="colonne_name" required>
-                <label for="colonne_type">Type de la colonne:</label>
-                <select name="colonne_type">
-                    <option value="INT(6)">INT(6)</option>
-                    <option value="VARCHAR(255)" selected>VARCHAR(255)</option>
-                    <option value="DATE">DATE</option>
-                    <!-- Ajoutez d'autres options de type de colonne si nécessaire -->
-                </select>
-                <input type="submit" value="Ajouter">
-            </form>
-
-            <!-- Formulaire de suppression de colonne -->
-            <h2>Supprimer une colonne</h2>
-            <form method="post" action="">
-                <input type="hidden" name="action" value="supprimer_colonne">
-                <label for="table_name">Table:</label>
-                <input type="text" name="table_name" required>
-                <label for="colonne_name">Nom de la colonne:</label>
-                <input type="text" name="colonne_name" required>
-                <input type="submit" value="Supprimer">
-            </form>
-
-            <!-- Formulaire de modification de colonne -->
-            <h2>Modifier une colonne</h2>
-            <form method="post" action="">
-                <input type="hidden" name="action" value="modifier_colonne">
-                <label for="table_name">Table:</label>
-                <input type="text" name="table_name" required>
-                <label for="colonne_name">Nom de la colonne:</label>
-                <input type="text" name="colonne_name" required>
-                <label for="nouveau_nom">Nouveau nom:</label>
-                <input type="text" name="nouveau_nom" required>
-                <label for="nouveau_type">Nouveau type:</label>
-                <select name="nouveau_type">
-                    <option value="INT(6)">INT(6)</option>
-                    <option value="VARCHAR(255)" selected>VARCHAR(255)</option>
-                    <option value="DATE">DATE</option>
-                    <!-- Ajoutez d'autres options de type de colonne si nécessaire -->
-                </select>
-                <input type="submit" value="Modifier">
-            </form>
-        </div>
-        <div class="right-section">
-            <!-- Affichage de la structure de la base de données -->
-            <h2>Structure de la base de données</h2>
-            <?php
-            $sql = "SHOW TABLES";
-            $result = mysqli_query($conn, $sql);
-
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $table = $row['Tables_in_' . $dbName];
-                    echo "<h3>$table</h3>";
-                    echo "<ul>";
-
-                    $sql_columns = "SHOW COLUMNS FROM $table";
-                    $result_columns = mysqli_query($conn, $sql_columns);
-
-                    if (mysqli_num_rows($result_columns) > 0) {
-                        while ($row_columns = mysqli_fetch_assoc($result_columns)) {
-                            echo "<li>" . $row_columns['Field'] . " (" . $row_columns['Type'] . ")</li>";
-                        }
+                    if (mysqli_query($conn, $sql)) {
+                        echo "La table $table_name a été ajoutée avec succès.";
                     } else {
-                        echo "<li>Aucune colonne trouvée.</li>";
+                        echo "Erreur lors de la création de la table: " . mysqli_error($conn);
                     }
+                    break;
 
-                    echo "</ul>";
-                }
-            } else {
-                echo "Aucune table trouvée dans la base de données.";
+                case 'supprimer_table':
+                    $table_name = $_POST['table_name'];
+
+                    // Suppression de la table
+                    $sql = "DROP TABLE $table_name";
+
+                    if (mysqli_query($conn, $sql)) {
+                        echo "La table $table_name a été supprimée avec succès.";
+                    } else {
+                        echo "Erreur lors de la suppression de la table: " . mysqli_error($conn);
+                    }
+                    break;
+
+                case 'ajouter_colonne':
+                    $table_name = $_POST['table_name'];
+                    $colonne_name = $_POST['colonne_name'];
+                    $colonne_type = $_POST['colonne_type'];
+
+                    // Ajout de la colonne dans la table
+                    $sql = "ALTER TABLE $table_name ADD COLUMN $colonne_name $colonne_type";
+
+                    if (mysqli_query($conn, $sql)) {
+                        echo "La colonne $colonne_name a été ajoutée avec succès à la table $table_name.";
+                    } else {
+                        echo "Erreur lors de l'ajout de la colonne: " . mysqli_error($conn);
+                    }
+                    break;
+
+                case 'supprimer_colonne':
+                    $table_name = $_POST['table_name'];
+                    $colonne_name = $_POST['colonne_name'];
+
+                    // Suppression de la colonne de la table
+                    $sql = "ALTER TABLE $table_name DROP COLUMN $colonne_name";
+
+                    if (mysqli_query($conn, $sql)) {
+                        echo "La colonne $colonne_name a été supprimée avec succès de la table $table_name.";
+                    } else {
+                        echo "Erreur lors de la suppression de la colonne: " . mysqli_error($conn);
+                    }
+                    break;
+
+                case 'modifier_colonne':
+                    $table_name = $_POST['table_name'];
+                    $colonne_name = $_POST['colonne_name'];
+                    $nouveau_nom = $_POST['nouveau_nom'];
+                    $nouveau_type = $_POST['nouveau_type'];
+
+                    // Modification de la colonne dans la table
+                    $sql = "ALTER TABLE $table_name CHANGE COLUMN $colonne_name $nouveau_nom $nouveau_type";
+
+                    if (mysqli_query($conn, $sql)) {
+                        echo "La colonne $colonne_name a été modifiée avec succès dans la table $table_name.";
+                    } else {
+                        echo "Erreur lors de la modification de la colonne: " . mysqli_error($conn);
+                    }
+                    break;
+
+                default:
+                    echo "Action non valide.";
+                    break;
             }
+        }
+        ?>
 
-            // Fermeture de la connexion à la base de données
-            mysqli_close($conn);
-            ?>
-        </div>
+        <!-- Formulaire d'ajout de table -->
+        <h2>Ajouter une table</h2>
+        <form method="post" action="">
+            <input type="hidden" name="action" value="ajouter_table">
+            <label for="table_name">Nom de la table:</label>
+            <input type="text" name="table_name" required>
+            <input type="submit" value="Ajouter">
+        </form>
+
+        <!-- Formulaire de suppression de table -->
+        <h2>Supprimer une table</h2>
+        <form method="post" action="">
+            <input type="hidden" name="action" value="supprimer_table">
+            <label for="table_name">Nom de la table:</label>
+            <input type="text" name="table_name" required>
+            <input type="submit" value="Supprimer">
+        </form>
+
+        <!-- Formulaire d'ajout de colonne -->
+        <h2>Ajouter une colonne</h2>
+        <form method="post" action="">
+            <input type="hidden" name="action" value="ajouter_colonne">
+            <label for="table_name">Table:</label>
+            <input type="text" name="table_name" required>
+            <label for="colonne_name">Nom de la colonne:</label>
+            <input type="text" name="colonne_name" required>
+            <label for="colonne_type">Type de la colonne:</label>
+            <select name="colonne_type">
+                <option value="INT(6)">INT(6)</option>
+                <option value="VARCHAR(255)" selected>VARCHAR(255)</option>
+                <option value="DATE">DATE</option>
+                <!-- Ajoutez d'autres options de type de colonne si nécessaire -->
+            </select>
+            <input type="submit" value="Ajouter">
+        </form>
+
+        <!-- Formulaire de suppression de colonne -->
+        <h2>Supprimer une colonne</h2>
+        <form method="post" action="">
+            <input type="hidden" name="action" value="supprimer_colonne">
+            <label for="table_name">Table:</label>
+            <input type="text" name="table_name" required>
+            <label for="colonne_name">Nom de la colonne:</label>
+            <input type="text" name="colonne_name" required>
+            <input type="submit" value="Supprimer">
+        </form>
+
+        <!-- Formulaire de modification de colonne -->
+        <h2>Modifier une colonne</h2>
+        <form method="post" action="">
+            <input type="hidden" name="action" value="modifier_colonne">
+            <label for="table_name">Table:</label>
+            <input type="text" name="table_name" required>
+            <label for="colonne_name">Nom de la colonne:</label>
+            <input type="text" name="colonne_name" required>
+            <label for="nouveau_nom">Nouveau nom:</label>
+            <input type="text" name="nouveau_nom" required>
+            <label for="nouveau_type">Nouveau type:</label>
+            <select name="nouveau_type">
+                <option value="INT(6)">INT(6)</option>
+                <option value="VARCHAR(255)" selected>VARCHAR(255)</option>
+                <option value="DATE">DATE</option>
+                <!-- Ajoutez d'autres options de type de colonne si nécessaire -->
+            </select>
+            <input type="submit" value="Modifier">
+        </form>
     </div>
+    <div class="right-section">
+        <!-- Affichage de la structure de la base de données -->
+        <h2>Structure de la base de données</h2>
+        <?php
+        $sql = "SHOW TABLES";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $table = $row['Tables_in_' . $dbName];
+                echo "<h3>$table</h3>";
+                echo "<ul>";
+
+                $sql_columns = "SHOW COLUMNS FROM $table";
+                $result_columns = mysqli_query($conn, $sql_columns);
+
+                if (mysqli_num_rows($result_columns) > 0) {
+                    while ($row_columns = mysqli_fetch_assoc($result_columns)) {
+                        echo "<li>" . $row_columns['Field'] . " (" . $row_columns['Type'] . ")</li>";
+                    }
+                } else {
+                    echo "<li>Aucune colonne trouvée.</li>";
+                }
+
+                echo "</ul>";
+            }
+        } else {
+            echo "Aucune table trouvée dans la base de données.";
+        }
+
+        // Fermeture de la connexion à la base de données
+        mysqli_close($conn);
+        ?>
+    </div>
+</div>
 </body>
 </html>
