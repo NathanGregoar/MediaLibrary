8539a4eab54bc9f9eab0baa0862ae7aec9c11150
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-06-09 13:29:05 +0200

    Update error.php

--- MediaLibrary/web/error/error.php
+++ MediaLibrary/web/error/error.php
@@ -11,11 +11,38 @@
         h1 {
             color: #ff0000;
         }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            background-color: #f5f5f5;
+        }
+
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #4caf50;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 4px;
+            transition: background-color 0.3s ease;
+        }
+
+        .button:hover {
+            background-color: #45a049;
+        }
     </style>
 </head>
 <body>
-    <h1>Erreur 404 - Page non trouvée</h1>
-    <p>La page que vous recherchez n'existe pas.</p>
-    <!-- Vous pouvez personnaliser davantage votre page d'erreur ici -->
+    <div class="container">
+        <h1>Erreur 404 - Page non trouvée</h1>
+        <p>La page que vous recherchez n'existe pas.</p>
+        <p>Nous sommes désolés, mais la page demandée n'a pas été trouvée.</p>
+        <p>Veuillez vérifier l'URL que vous avez saisie ou cliquez sur le bouton ci-dessous pour retourner à la page d'accueil.</p>
+        <a class="button" href="../accueil/index.php">Retour à la page d'accueil</a>
+    </div>
 </body>
 </html>
