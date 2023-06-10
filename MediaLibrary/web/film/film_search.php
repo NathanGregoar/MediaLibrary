e1ca83a8226413f77f712fa04deb8623ead96429
NathanGregoar <he201832@students.ephec.be>
2023-06-11 01:28:24 +0200

    style film

--- MediaLibrary/web/film/film_search.php
+++ MediaLibrary/web/film/film_search.php
@@ -55,7 +55,7 @@ $connection->close();
 
     <h1>Rechercher des Films</h1>
 
-    <div class="container">
+    <div class="container_search">
         <div class="search-bar">
             <form method="GET">
                 <input type="text" name="search" placeholder="Rechercher un film" style="width: 100%; padding: 10px; margin-bottom: 10px; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;">
