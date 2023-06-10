3c97b697d8a43024e4bde7f98325277e335d8ba1
NathanGregoar <he201832@students.ephec.be>
2023-06-10 12:14:14 +0200

    Update film.php

--- MediaLibrary/web/film/film.php
+++ MediaLibrary/web/film/film.php
@@ -1,30 +1,6 @@
 <?php
 require_once '../utils/auth.php';
 require_once '../utils/config.php';
-
-$connection = mysqli_connect($host, $username, $password, $dbName);
-
-if (!$connection) {
-    die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
-}
-
-// Ajout d'un film
-if (isset($_POST['title'])) {
-    $title = $connection->real_escape_string($_POST['title']);
-    $director = $connection->real_escape_string($_POST['director']);
-    $releaseYear = $connection->real_escape_string($_POST['release_year']);
-    $externalHardDrive = $connection->real_escape_string($_POST['external_hard_drive']);
-
-    $query = "INSERT INTO films (title, director, release_year, external_hard_drive) VALUES ('$title', '$director', '$releaseYear', '$externalHardDrive')";
-
-    if (mysqli_query($connection, $query)) {
-        echo 'Le film a été ajouté avec succès.';
-    } else {
-        echo 'Erreur lors de l\'ajout du film : ' . mysqli_error($connection);
-    }
-}
-
-mysqli_close($connection);
 ?>
 
 <!DOCTYPE html>
@@ -32,51 +8,6 @@ mysqli_close($connection);
 <head>
     <title>Ajouter un Film</title>
     <link rel="stylesheet" type="text/css" href="./film.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $('#title').on('blur', function() {
-                var title = $(this).val();
-                var director = $('#director').val();
-
-                if (title !== '' && director !== '') {
-                    $.ajax({
-                        url: 'get_film_info.php',
-                        method: 'GET',
-                        data: { title: title, director: director },
-                        dataType: 'json',
-                        success: function(response) {
-                            if (response) {
-                                $('#director').val(response.director);
-                                $('#release_year').val(response.releaseYear);
-                                $('#external_hard_drive').val(response.externalHardDrive);
-                            }
-                        }
-                    });
-                }
-            });
-
-            $('#director').on('blur', function() {
-                var title = $('#title').val();
-                var director = $(this).val();
-
-                if (title !== '' && director !== '') {
-                    $.ajax({
-                        url: 'get_film_info.php',
-                        method: 'GET',
-                        data: { title: title, director: director },
-                        dataType: 'json',
-                        success: function(response) {
-                            if (response) {
-                                $('#release_year').val(response.releaseYear);
-                                $('#external_hard_drive').val(response.externalHardDrive);
-                            }
-                        }
-                    });
-                }
-            });
-        });
-    </script>
 </head>
 <body>
     <div class="container">
@@ -88,21 +19,62 @@ mysqli_close($connection);
 
         <h1>Ajouter un Film</h1>
 
-        <form method="POST" action="">
+        <?php
+        $connection = mysqli_connect($host, $username, $password, $dbName);
+
+        if (!$connection) {
+            die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
+        }
+
+        // Ajout d'un film
+        if (isset($_POST['title'])) {
+            $title = $connection->real_escape_string($_POST['title']);
+            $director = $_POST['director'] != '' ? $connection->real_escape_string($_POST['director']) : 'NULL';
+            $releaseYear = $_POST['release_year'] != '' ? $connection->real_escape_string($_POST['release_year']) : 'NULL';
+            $externalHardDrive = $_POST['external_hard_drive'] != '' ? intval($_POST['external_hard_drive']) : 'NULL';
+        
+            // Récupérer l'ID de l'utilisateur connecté à partir des informations de session
+            $loggedInUser = getLoggedInUser();
+        
+            // Vérifier les doublons
+            $duplicateSql = "SELECT * FROM films WHERE title = '$title' AND director = '$director' AND release_year = '$releaseYear' AND added_by = " . $loggedInUser['id'];
+            $duplicateResult = $connection->query($duplicateSql);
+        
+            if ($duplicateResult->num_rows > 0) {
+                echo '<div class="alert alert-error">Le film existe déjà dans la base de données.</div>';
+            } else {
+                $insertSql = "INSERT INTO films (title, director, release_year, external_hard_drive, added_by) VALUES (?, ?, ?, ?, ?)";
+                $insertStmt = $connection->prepare($insertSql);
+                $insertStmt->bind_param("ssisi", $title, $director, $releaseYear, $externalHardDrive, $loggedInUser['id']);
+        
+                if ($insertStmt->execute()) {
+                    echo '<div class="alert alert-success">Film ajouté avec succès !</div>';
+                } else {
+                    echo '<div class="alert alert-error">Erreur lors de l\'ajout du film : ' . $connection->error . '</div>';
+                }
+            }
+        }
+        ?>
+
+        <form method="POST">
             <label for="title">Titre :</label>
-            <input type="text" id="title" name="title" required><br><br>
+            <input type="text" id="title" name="title" required>
 
             <label for="director">Réalisateur :</label>
-            <input type="text" id="director" name="director" required><br><br>
+            <input type="text" id="director" name="director">
 
             <label for="release_year">Année de sortie :</label>
-            <input type="number" id="release_year" name="release_year" required><br><br>
+            <input type="text" id="release_year" name="release_year">
 
-            <label for="external_hard_drive">Disque dur externe :</label>
-            <input type="text" id="external_hard_drive" name="external_hard_drive"><br><br>
+            <label for="external_hard_drive">Numéro du disque dur externe :</label>
+            <input type="text" id="external_hard_drive" name="external_hard_drive">
 
             <input type="submit" value="Ajouter">
         </form>
+
+        <?php
+        $connection->close();
+        ?>
     </div>
 </body>
 </html>
