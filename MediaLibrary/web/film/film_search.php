408ef98da9e65041a3f795ebcb8410e99ca8ff37
NathanGregoar <he201832@students.ephec.be>
2023-06-10 14:44:00 +0200

    Update film_search.php

--- MediaLibrary/web/film/film_search.php
+++ MediaLibrary/web/film/film_search.php
@@ -19,9 +19,9 @@ $loggedInUser = getLoggedInUser();
         <a href="./film_search.php">Consulter les Films</a>
     </div>
 
-    <div class="container">
-        <h1>Rechercher des Films</h1>
+    <h1>Rechercher des Films</h1>
 
+    <div class="container">
         <div class="search-bar">
             <form method="GET">
                 <input type="text" name="search" placeholder="Rechercher un film" style="width: 100%; padding: 10px; margin-bottom: 10px; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;">
