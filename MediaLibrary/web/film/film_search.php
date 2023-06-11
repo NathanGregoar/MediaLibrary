2a1c5a4d74c09066a9f43ec3c3e28a6749970028
NathanGregoar <he201832@students.ephec.be>
2023-06-11 02:25:27 +0200

    modifier film

--- MediaLibrary/web/film/film_search.php
+++ MediaLibrary/web/film/film_search.php
@@ -27,6 +27,23 @@ if (isset($_POST['delete'])) {
     }
 }
 
+// Modification d'un film
+if (isset($_POST['edit'])) {
+    $editId = $connection->real_escape_string($_POST['edit']);
+    $title = $connection->real_escape_string($_POST['title']);
+    $director = $connection->real_escape_string($_POST['director']);
+    $releaseYear = $connection->real_escape_string($_POST['release_year']);
+    $externalHardDrive = $connection->real_escape_string($_POST['external_hard_drive']);
+
+    $updateSql = "UPDATE films SET title = '$title', director = '$director', release_year = '$releaseYear', external_hard_drive = '$externalHardDrive' WHERE id = $editId AND added_by = " . $loggedInUser['id'];
+
+    if ($connection->query($updateSql) === TRUE) {
+        echo '<div class="alert alert-success">Film modifié avec succès !</div>';
+    } else {
+        echo '<div class="alert alert-error">Erreur lors de la modification du film : ' . $connection->error . '</div>';
+    }
+}
+
 // Récupération des films correspondant à la recherche
 $searchResult = $connection->query($searchSql);
 $numSearchResults = $searchResult->num_rows;
@@ -110,7 +127,7 @@ $connection->close();
                             </form>
 
                             <form method="POST" action="modifier_film.php" style="display:inline">
-                                <input type="hidden" name="id" value="<?php echo $id; ?>">
+                                <input type="hidden" name="edit" value="<?php echo $id; ?>">
                                 <input type="hidden" name="title" value="<?php echo $title; ?>">
                                 <input type="hidden" name="director" value="<?php echo $director; ?>">
                                 <input type="hidden" name="release_year" value="<?php echo $releaseYear; ?>">
@@ -159,7 +176,7 @@ $connection->close();
                         </form>
 
                         <form method="POST" action="modifier_film.php" style="display:inline">
-                            <input type="hidden" name="id" value="<?php echo $id; ?>">
+                            <input type="hidden" name="edit" value="<?php echo $id; ?>">
                             <input type="hidden" name="title" value="<?php echo $title; ?>">
                             <input type="hidden" name="director" value="<?php echo $director; ?>">
                             <input type="hidden" name="release_year" value="<?php echo $releaseYear; ?>">
