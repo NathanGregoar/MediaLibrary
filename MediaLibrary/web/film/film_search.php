7d4200e30b924352837e1d61e61fe627afd7890d
NathanGregoar <he201832@students.ephec.be>
2023-06-12 00:19:09 +0200

    Modif style

--- MediaLibrary/web/film/film_search.php
+++ MediaLibrary/web/film/film_search.php
@@ -1,112 +1,38 @@
-<?php
-require_once '../utils/auth.php';
-require_once '../utils/config.php';
-
-// Récupérer l'utilisateur connecté
-$loggedInUser = getLoggedInUser();
-
-// Définition des variables de recherche
-$searchTerm = isset($_GET['search']) ? $_GET['search'] : '';
-$searchSql = "SELECT * FROM films WHERE title LIKE '%$searchTerm%' AND added_by = " . $loggedInUser['id'];
-
-// Connexion à la base de données
-$connection = mysqli_connect($host, $username, $password, $dbName);
-if (!$connection) {
-    die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
-}
-
-// Suppression d'un film
-if (isset($_POST['delete'])) {
-    $deleteId = $connection->real_escape_string($_POST['delete']);
-    $deleteSql = "DELETE FROM films WHERE id = $deleteId AND added_by = " . $loggedInUser['id'];
-
-    if ($connection->query($deleteSql) === TRUE) {
-        $deleteAlert = '<div class="alert alert-success">Film supprimé avec succès !</div>';
-    } else {
-        $deleteAlert = '<div class="alert alert-error">Erreur lors de la suppression du film : ' . $connection->error . '</div>';
-    }
-}
-
-// Mise à jour d'un film
-if (isset($_POST['update'])) {
-    $updateId = $connection->real_escape_string($_POST['update']);
-    $title = $connection->real_escape_string($_POST['title']);
-    $director = $connection->real_escape_string($_POST['director']);
-    $releaseYear = $connection->real_escape_string($_POST['release_year']);
-    $externalHardDrive = $connection->real_escape_string($_POST['external_hard_drive']);
-
-    $updateSql = "UPDATE films SET title = '$title', director = '$director', release_year = '$releaseYear', external_hard_drive = '$externalHardDrive' WHERE id = $updateId AND added_by = " . $loggedInUser['id'];
-
-    if ($connection->query($updateSql) === TRUE) {
-        $updateAlert = '<div class="alert alert-success">Film mis à jour avec succès !</div>';
-    } else {
-        $updateAlert = '<div class="alert alert-error">Erreur lors de la mise à jour du film : ' . $connection->error . '</div>';
-    }
-}
-
-// Récupération des films correspondant à la recherche
-$searchResult = $connection->query($searchSql);
-$numSearchResults = $searchResult->num_rows;
-
-// Récupération de tous les films ajoutés par l'utilisateur connecté
-$userMoviesSql = "SELECT * FROM films WHERE added_by = " . $loggedInUser['id'];
-$userMoviesResult = $connection->query($userMoviesSql);
-$numUserMovies = $userMoviesResult->num_rows;
-
-// Fermeture de la connexion à la base de données
-$connection->close();
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="fr">
+
 <head>
-    <title>Rechercher des Films</title>
-    <link rel="stylesheet" type="text/css" href="film.css">
-    <style>
-        .update-form {
-            display: none;
-        }
-    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body>
-    <div class="navbar">
-        <a href="../accueil/index.php">Accueil</a>
-        <a href="./film.php">Ajouter un Film</a>
-        <a href="./film_search.php">Consulter les Films</a>
-    </div>
+    <div class="container">
+        <h1>Gestionnaire de films</h1>
 
-    <h1>Rechercher des Films</h1>
-
-    <div class="alert-container">
-        <?php if ($searchTerm !== '') : ?>
-            <?php if ($numSearchResults > 0) : ?>
-                <div class="alert alert-success">Résultats de la recherche (<?php echo $numSearchResults; ?>) :</div>
-            <?php else : ?>
-                <div class="alert alert-info">Aucun résultat trouvé pour la recherche "<?php echo $searchTerm; ?>"</div>
-            <?php endif; ?>
+        <?php if (!empty($successMessage)) : ?>
+            <div class="alert-container">
+                <div class="alert alert-success"><?php echo $successMessage; ?></div>
+            </div>
         <?php endif; ?>
 
-        <?php if (isset($deleteAlert)) : ?>
-            <?php echo $deleteAlert; ?>
+        <?php if (!empty($errorMessage)) : ?>
+            <div class="alert-container">
+                <div class="alert alert-info"><?php echo $errorMessage; ?></div>
+            </div>
         <?php endif; ?>
 
-        <?php if (isset($updateAlert)) : ?>
-            <?php echo $updateAlert; ?>
-        <?php endif; ?>
-    </div>
+        <div class="container_search">
+            <div class="search-bar">
+                <form method="POST">
+                    <input type="text" name="search" placeholder="Rechercher un film...">
+                    <input type="submit" value="Rechercher">
+                </form>
+            </div>
 
-    <div class="container_search">
-        <div class="search-bar">
-            <form method="GET">
-                <input type="text" name="search" placeholder="Rechercher un film" style="width: 100%; padding: 10px; margin-bottom: 10px; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;">
-                <input type="submit" value="Rechercher" style="padding: 10px 20px; background-color: #4CAF50; color: white; border: none; border-radius: 4px; cursor: pointer;">
-            </form>
-        </div>
-
-        <?php if ($searchTerm !== '') : ?>
-            <h2>Résultats de la recherche (<?php echo $numSearchResults; ?>) :</h2>
+            <h2>Tous les films (<?php echo $numMovies; ?>) :</h2>
             <div class="movies-list">
-                <?php while ($row = $searchResult->fetch_assoc()) : ?>
+                <?php while ($row = $moviesResult->fetch_assoc()) : ?>
                     <?php
                     $id = $row['id'];
                     $title = $row['title'];
@@ -133,65 +59,79 @@ $connection->close();
                             <p><strong>Réalisateur :</strong> <?php echo ($director != 'NULL' ? $director : ''); ?></p>
                             <p><strong>Année de sortie :</strong> <?php echo ($releaseYear != 'NULL' ? $releaseYear : ''); ?></p>
                             <p><strong>Disque dur externe :</strong> <?php echo ($externalHardDrive != 'NULL' ? $externalHardDrive : ''); ?></p>
-
-                            <form method="POST" style="display:inline">
-                                <input type="hidden" name="delete" value="<?php echo $id; ?>">
-                                <input type="submit" value="Supprimer" class="delete-btn">
-                            </form>
-                            
-                            <form method="POST" style="display:inline">
-                                <input type="hidden" name="edit" value="<?php echo $id; ?>">
-                                <input type="submit" value="Modifier" class="edit-btn">
-                            </form>
+                        </div>
+                        <div class="actions">
+                            <button class="edit-btn" data-id="<?php echo $id; ?>">Modifier</button>
+                            <button class="delete-btn" data-id="<?php echo $id; ?>">Supprimer</button>
                         </div>
                     </div>
                 <?php endwhile; ?>
             </div>
-        <?php endif; ?>
+        </div>
 
-        <h2>Vos films (<?php echo $numUserMovies; ?>) :</h2>
-        <div class="movies-list">
-            <?php while ($row = $userMoviesResult->fetch_assoc()) : ?>
-                <?php
-                $id = $row['id'];
-                $title = $row['title'];
-                $director = $row['director'];
-                $releaseYear = $row['release_year'];
-                $externalHardDrive = $row['external_hard_drive'];
-
-                // Appel à l'API OMDB pour récupérer les informations du film
-                $apiUrl = "http://www.omdbapi.com/?apikey=f1e681ff&t=" . urlencode($title);
-                $response = file_get_contents($apiUrl);
-                $data = json_decode($response, true);
-
-                // Vérifier si la requête a réussi et si l'affiche est disponible
-                if ($data['Response'] === 'True' && $data['Poster'] !== 'N/A') {
-                    $poster = $data['Poster'];
-                } else {
-                    $poster = 'placeholder.png'; // Affiche par défaut en cas d'erreur ou d'affiche indisponible
-                }
-                ?>
-                <div class="movie-item">
-                    <img src="<?php echo $poster; ?>" alt="<?php echo $title; ?>">
-                    <div class="movie-details">
-                        <h3><?php echo $title; ?></h3>
-                        <p><strong>Réalisateur :</strong> <?php echo ($director != 'NULL' ? $director : ''); ?></p>
-                        <p><strong>Année de sortie :</strong> <?php echo ($releaseYear != 'NULL' ? $releaseYear : ''); ?></p>
-                        <p><strong>Disque dur externe :</strong> <?php echo ($externalHardDrive != 'NULL' ? $externalHardDrive : ''); ?></p>
-
-                        <form method="POST" style="display:inline">
-                            <input type="hidden" name="delete" value="<?php echo $id; ?>">
-                            <input type="submit" value="Supprimer" class="delete-btn">
-                        </form>
-                        
-                        <form method="POST" style="display:inline">
-                            <input type="hidden" name="edit" value="<?php echo $id; ?>">
-                            <input type="submit" value="Modifier" class="edit-btn">
-                        </form>
-                    </div>
-                </div>
-            <?php endwhile; ?>
+        <div class="edit-form-container">
+            <div class="edit-form-box">
+                <button class="close-btn">Fermer</button>
+                <h2>Modifier le film</h2>
+                <form id="edit-form" method="POST">
+                    <input type="hidden" id="edit-movie-id" name="edit_movie_id">
+                    <label for="edit-movie-title">Titre :</label>
+                    <input type="text" id="edit-movie-title" name="edit_movie_title" required>
+                    <label for="edit-movie-director">Réalisateur :</label>
+                    <input type="text" id="edit-movie-director" name="edit_movie_director" required>
+                    <label for="edit-movie-release-year">Année de sortie :</label>
+                    <input type="text" id="edit-movie-release-year" name="edit_movie_release_year" required>
+                    <label for="edit-movie-external-hard-drive">Disque dur externe :</label>
+                    <input type="text" id="edit-movie-external-hard-drive" name="edit_movie_external_hard_drive" required>
+                    <input type="submit" value="Enregistrer">
+                </form>
+            </div>
         </div>
     </div>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var editFormContainer = document.querySelector(".edit-form-container");
+            var editFormBox = document.querySelector(".edit-form-box");
+            var closeBtn = document.querySelector(".close-btn");
+            var editBtns = document.querySelectorAll(".edit-btn");
+            var editForm = document.getElementById("edit-form");
+            var editMovieId = document.getElementById("edit-movie-id");
+            var editMovieTitle = document.getElementById("edit-movie-title");
+            var editMovieDirector = document.getElementById("edit-movie-director");
+            var editMovieReleaseYear = document.getElementById("edit-movie-release-year");
+            var editMovieExternalHardDrive = document.getElementById("edit-movie-external-hard-drive");
+
+            editBtns.forEach(function(btn) {
+                btn.addEventListener("click", function() {
+                    var movieId = btn.getAttribute("data-id");
+                    var movieItem = btn.closest(".movie-item");
+                    var movieTitle = movieItem.querySelector("h3").textContent;
+                    var movieDirector = movieItem.querySelector("p:nth-child(2)").textContent.split(": ")[1];
+                    var movieReleaseYear = movieItem.querySelector("p:nth-child(3)").textContent.split(": ")[1];
+                    var movieExternalHardDrive = movieItem.querySelector("p:nth-child(4)").textContent.split(": ")[1];
+
+                    editMovieId.value = movieId;
+                    editMovieTitle.value = movieTitle;
+                    editMovieDirector.value = movieDirector;
+                    editMovieReleaseYear.value = movieReleaseYear;
+                    editMovieExternalHardDrive.value = movieExternalHardDrive;
+
+                    editFormContainer.style.display = "flex";
+                    setTimeout(function() {
+                        editFormBox.style.transform = "translateY(0)";
+                    }, 10);
+                });
+            });
+
+            closeBtn.addEventListener("click", function() {
+                editFormBox.style.transform = "translateY(-100%)";
+                setTimeout(function() {
+                    editFormContainer.style.display = "none";
+                }, 200);
+            });
+        });
+    </script>
 </body>
+
 </html>
