4c85d4d0f94fd25d90a38740406aa725ef086007
NathanGregoar <he201832@students.ephec.be>
2023-06-17 00:13:52 +0200

    style livre

--- MediaLibrary/web/livres/livres_souhaite_search.php
+++ MediaLibrary/web/livres/livres_souhaite_search.php
@@ -34,7 +34,7 @@ $loggedInUser = getLoggedInUser();
 
     <h1>Rechercher des Livres</h1>
 
-    <div class="container">
+    <div class="container_search">
         <div class="search-bar">
             <form method="GET">
                 <input type="text" name="search" placeholder="Rechercher un livre" style="width: 100%; padding: 10px; margin-bottom: 10px; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;">
