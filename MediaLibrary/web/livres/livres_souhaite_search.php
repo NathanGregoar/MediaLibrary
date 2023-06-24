01449a4124261291d07bd30060c2caed36c380b1
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-06-24 17:50:30 +0200

    Update livres_souhaite_search.php

--- MediaLibrary/web/livres/livres_souhaite_search.php
+++ MediaLibrary/web/livres/livres_souhaite_search.php
@@ -14,6 +14,10 @@ function getBookCover($title, $author) {
     return null;
 }
 
+if (!$connection) {
+    die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
+}
+
 // Modification d'un livre souhaité
 if (isset($_POST['edit'])) {
     $editId = $connection->real_escape_string($_POST['edit']);
