f9dd5e4493f0a1b5b4ae1d95bdcebe2b5c7aa99e
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-06-24 17:03:41 +0200

    Update livres_souhaite_search.php

--- MediaLibrary/web/livres/livres_souhaite_search.php
+++ MediaLibrary/web/livres/livres_souhaite_search.php
@@ -14,6 +14,47 @@ function getBookCover($title, $author) {
     return null;
 }
 
+// Modification d'un livre souhaité
+if (isset($_POST['edit'])) {
+    $editId = $connection->real_escape_string($_POST['edit']);
+    $editSql = "SELECT * FROM livres_souhaites WHERE id = $editId AND added_by = " . $loggedInUser['id'];
+    $editResult = $connection->query($editSql);
+
+    if ($editResult->num_rows > 0) {
+        $editData = $editResult->fetch_assoc();
+        $editTitle = $editData['titre'];
+        $editAuteur = $editData['auteur'];
+        $editNumeroTome = $editData['numero_tome'];
+        $editNombreTotalTomes = $editData['nombre_total_tomes'];
+        $editPrix = $editData['prix'];
+        $editFormat = $editData['format'];
+        $editMaisonEdition = $editData['maison_edition'];
+        $editResumeLivre = $editData['resume_livre'];
+        $editFormVisible = true;
+    }
+}
+
+// Mise à jour d'un livre souhaité
+if (isset($_POST['update'])) {
+    $updateId = $connection->real_escape_string($_POST['livre_id']);
+    $updateTitle = $connection->real_escape_string($_POST['titre']);
+    $updateAuteur = $connection->real_escape_string($_POST['auteur']);
+    $updateNumeroTome = $connection->real_escape_string($_POST['numero_tome']);
+    $updateNombreTotalTomes = $connection->real_escape_string($_POST['nombre_total_tomes']);
+    $updatePrix = $connection->real_escape_string($_POST['prix']);
+    $updateFormat = $connection->real_escape_string($_POST['format']);
+    $updateMaisonEdition = $connection->real_escape_string($_POST['maison_edition']);
+    $updateResumeLivre = $connection->real_escape_string($_POST['resume_livre']);
+
+    $updateSql = "UPDATE livres_souhaites SET titre = '$updateTitle', auteur = '$updateAuteur', numero_tome = '$updateNumeroTome', nombre_total_tomes = '$updateNombreTotalTomes', prix = '$updatePrix', format = '$updateFormat', maison_edition = '$updateMaisonEdition', resume_livre = '$updateResumeLivre' WHERE id = $updateId AND added_by = " . $loggedInUser['id'];
+
+    if ($connection->query($updateSql) === TRUE) {
+        $updateAlert = '<div class="alert alert-success">Livre souhaité mis à jour avec succès !</div>';
+    } else {
+        $updateAlert = '<div class="alert alert-error">Erreur lors de la mise à jour du livre souhaité : ' . $connection->error . '</div>';
+    }
+}
+
 // Récupérer l'utilisateur connecté
 $loggedInUser = getLoggedInUser();
 ?>
@@ -161,5 +202,62 @@ $loggedInUser = getLoggedInUser();
         mysqli_close($connection);
         ?>
     </div>
+
+    <div id="edit-form-container" style="display: none;">
+        <h2>Modifier un livre souhaité</h2>
+        <form method="POST" action="livre_search.php">
+            <input type="hidden" name="livre_id" id="edit-livre-id">
+            <label for="edit-livre-titre">Titre :</label>
+            <input type="text" name="titre" id="edit-livre-titre">
+            <label for="edit-livre-auteur">Auteur :</label>
+            <input type="text" name="auteur" id="edit-livre-auteur">
+            <label for="edit-livre-numero-tome">Numéro de tome :</label>
+            <input type="text" name="numero_tome" id="edit-livre-numero-tome">
+            <label for="edit-livre-nombre-total-tomes">Nombre total de tomes :</label>
+            <input type="text" name="nombre_total_tomes" id="edit-livre-nombre-total-tomes">
+            <label for="edit-livre-prix">Prix :</label>
+            <input type="text" name="prix" id="edit-livre-prix">
+            <label for="edit-livre-format">Format :</label>
+            <input type="text" name="format" id="edit-livre-format">
+            <label for="edit-livre-maison-edition">Maison d'édition :</label>
+            <input type="text" name="maison_edition" id="edit-livre-maison-edition">
+            <label for="edit-livre-resume-livre">Résumé :</label>
+            <textarea name="resume_livre" id="edit-livre-resume-livre"></textarea>
+            <input type="submit" name="update" value="Enregistrer">
+        </form>
+    </div>
+
+    <script>
+        function showEditForm(livreId, livreTitre, livreAuteur, livreNumeroTome, livreNombreTotalTomes, livrePrix, livreFormat, livreMaisonEdition, livreResumeLivre) {
+            var editFormContainer = document.getElementById('edit-form-container');
+            editFormContainer.style.display = 'block';
+
+            var livreIdInput = document.getElementById('edit-livre-id');
+            livreIdInput.value = livreId;
+
+            var livreTitreInput = document.getElementById('edit-livre-titre');
+            var livreAuteurInput = document.getElementById('edit-livre-auteur');
+            var livreNumeroTomeInput = document.getElementById('edit-livre-numero-tome');
+            var livreNombreTotalTomesInput = document.getElementById('edit-livre-nombre-total-tomes');
+            var livrePrixInput = document.getElementById('edit-livre-prix');
+            var livreFormatInput = document.getElementById('edit-livre-format');
+            var livreMaisonEditionInput = document.getElementById('edit-livre-maison-edition');
+            var livreResumeLivreInput = document.getElementById('edit-livre-resume-livre');
+
+            // Remplir le formulaire avec les informations du livre correspondant à l'ID
+            livreTitreInput.value = livreTitre;
+            livreAuteurInput.value = livreAuteur;
+            livreNumeroTomeInput.value = livreNumeroTome;
+            livreNombreTotalTomesInput.value = livreNombreTotalTomes;
+            livrePrixInput.value = livrePrix;
+            livreFormatInput.value = livreFormat;
+            livreMaisonEditionInput.value = livreMaisonEdition;
+            livreResumeLivreInput.value = livreResumeLivre;
+
+            var livreIdDisplay = document.getElementById('edit-livre-id-display');
+            livreIdDisplay.textContent = livreId;
+        }
+    </script>
+
 </body>
 </html>
