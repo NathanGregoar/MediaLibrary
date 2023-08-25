df75412dd034fb5f258b12556e8f5339f8854cfb
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-08-25 17:33:42 +0200

    Update olympe.php

--- MediaLibrary/web/olympe/olympe.php
+++ MediaLibrary/web/olympe/olympe.php
@@ -19,7 +19,7 @@ if ($username !== "Nathan" || $email !== "nathan.gregoar@yahoo.fr") {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>L'Olympe</title>
+    <title>L'Olympe - Choix de destination</title>
     <link rel="stylesheet" type="text/css" href="./olympe.css">
 </head>
 <body>
@@ -28,5 +28,7 @@ if ($username !== "Nathan" || $email !== "nathan.gregoar@yahoo.fr") {
         <a href="../olympe/olympe.php" style="color: #D7EBF3;">L'Olympe</a>
         <a href="../ecollyday/ecollyday.php">Ecollyday</a>        
     </div>
+    <h1>Bienvenue dans l'Olympe <?php echo $username; ?></h1>
+    <h2>Choix de destination 2024</h2>
 </body>
 </html>
