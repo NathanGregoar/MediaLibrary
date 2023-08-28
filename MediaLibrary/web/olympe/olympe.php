ea3d921dc0b3a7f74f61409024dcdab0078c55a4
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-08-28 02:02:02 +0200

    Update olympe.php

--- MediaLibrary/web/olympe/olympe.php
+++ MediaLibrary/web/olympe/olympe.php
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     // Préparation de la requête d'insertion
     $insert_query = "INSERT INTO olympe (added_by, budget_min, budget_max, dispo, indispo, transport, pays_oui, pays_non)
-                     VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
     $stmt = $connection->prepare($insert_query);
     $stmt->bind_param("iiisssss", $loggedInUser['id'], $budget_min, $budget_max, $dispo_dates, $not_dispo_dates, $transport, $pref_countries_str, $non_pref_countries_str);
