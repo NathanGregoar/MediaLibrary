decefc6e11e2188f297e2a5f1433223811a83f02
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-08-28 22:52:43 +0200

    Update olympe.php

--- MediaLibrary/web/olympe/olympe.php
+++ MediaLibrary/web/olympe/olympe.php
@@ -84,15 +84,28 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 <label for="budget_min">Budget min :</label>
                 <small>(Transport & Location compris)</small>
                 <input type="number" id="budget_min" name="budget_min" min="1" required><br>
+
                 <label for="choosePrefCountriesBtn">Pays où j'ai envie de partir :</label>
                 <small>(5 Pays Max)</small>
                 <button type="button" id="choosePrefCountriesBtn">Choisir</button>
-                <input type="hidden" name="pref_countries_selected" id="pref_countries_selected">
+                <input type="hidden" name="pref_countries_selected" id="pref_countries_selected"><br>
+
+                <label>Transport allé-retour :</label>
+                <small>(Coché : Accepté / Pas coché : Refusé)</small>
+                <input type="checkbox" id="train" name="transport[]" value="train" class="transport-checkbox">
+                <label for="train" class="transport-label">Train</label>
+                <input type="checkbox" id="avion" name="transport[]" value="avion" class="transport-checkbox">
+                <label for="avion" class="transport-label">Avion</label>
+                <input type="checkbox" id="bus" name="transport[]" value="bus" class="transport-checkbox">
+                <label for="bus" class="transport-label">Bus</label>
+                <input type="checkbox" id="bateau" name="transport[]" value="bateau" class="transport-checkbox">
+                <label for="bateau" class="transport-label">Bateau</label>
             </div>
             <div class="input-group">
                 <label for="budget_max">Budget max :</label>
                 <small>(Transport & Location compris)</small>
                 <input type="number" id="budget_max" name="budget_max" min="1" required><br>
+                
                 <label for="chooseNonPrefCountriesBtn">Pays où je ne veux pas partir :</label>
                 <small>(3 Pays Max)</small>
                 <button type="button" id="chooseNonPrefCountriesBtn">Choisir</button>
@@ -106,7 +119,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 <label for="not_dispo_date">Mes indisponibilités :</label>
                 <input type="text" id="not_dispo_date" name="not_dispo_date" class="flatpickr inline" required>
             </div>
-            <div class="transport-group" style="grid-column: span 2;">
+            <!-- <div class="transport-group" style="grid-column: span 2;">
                 <label>Transport allé-retour :</label>
                 <small>(Coché : Accepté / Pas coché : Refusé)</small>
                 <input type="checkbox" id="train" name="transport[]" value="train" class="transport-checkbox">
@@ -117,7 +130,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 <label for="bus" class="transport-label">Bus</label>
                 <input type="checkbox" id="bateau" name="transport[]" value="bateau" class="transport-checkbox">
                 <label for="bateau" class="transport-label">Bateau</label>
-            </div>
+            </div> -->
         </div>
 
         <div class="centered">
