341023afc6dae0287e5eea268699fd89c72bb6d8
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-08-29 13:06:13 +0200

    Update olympe.php

--- MediaLibrary/web/olympe/olympe.php
+++ MediaLibrary/web/olympe/olympe.php
@@ -257,6 +257,36 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                 input.classList.add('valid');
             }
         }
+
+        // Vérification des cases check pays
+        const casesPaysPreferes = document.querySelectorAll('[name="pref_countries[]"]');
+        const casesPaysNonPreferes = document.querySelectorAll('[name="non_pref_countries[]"]');
+
+        casesPaysPreferes.forEach(casePaysPreferes => {
+            casePaysPreferes.addEventListener('change', function () {
+                const value = this.value;
+
+                // Désactiver les cases correspondantes dans Pays où je ne veux pas aller
+                casesPaysNonPreferes.forEach(casePaysNonPreferes => {
+                    if (casePaysNonPreferes.value === value) {
+                        casePaysNonPreferes.disabled = this.checked;
+                    }
+                });
+            });
+        });
+
+        casesPaysNonPreferes.forEach(casePaysNonPreferes => {
+            casePaysNonPreferes.addEventListener('change', function () {
+                const value = this.value;
+
+                // Désactiver les cases correspondantes dans Pays où je veux aller
+                casesPaysPreferes.forEach(casePaysPreferes => {
+                    if (casePaysPreferes.value === value) {
+                        casePaysPreferes.disabled = this.checked;
+                    }
+                });
+            });
+        });
     </script>
 
     <!-- Vérification du form -->
