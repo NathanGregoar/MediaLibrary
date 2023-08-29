aeda373bb06f4cb1207dd950baefeb70c75c93b2
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-08-29 18:16:19 +0200

    s

--- MediaLibrary/web/olympe/statchoixpays/stats.php
+++ MediaLibrary/web/olympe/statchoixpays/stats.php
@@ -106,15 +106,17 @@ $connection->close();
     <h1>Bienvenue dans l'Olympe <?php echo $username;?> - Stats choix de la destination Summer 2024</h1>
     <h2><?php echo $totalGods . " " . $text; ?> au formulaire !</h2>
 
-    <fieldset style="max-width: 20%;">
-        <legend>Pays où l'Olympe veut partir :</legend>
-        <canvas id="pieChartPaysOui"></canvas>
-    </fieldset>
+    <div class="fixed-diagrams">
+        <fieldset style="max-width: 20%;">
+            <legend>Pays où l'Olympe veut partir :</legend>
+            <canvas id="pieChartPaysOui"></canvas>
+        </fieldset>
 
-    <fieldset style="max-width: 20%;">
-        <legend>Pays où l'Olympe ne veut pas partir :</legend>
-        <canvas id="pieChartPaysNon"></canvas>
-    </fieldset>
+        <fieldset style="max-width: 20%;">
+            <legend>Pays où l'Olympe ne veut pas partir :</legend>
+            <canvas id="pieChartPaysNon"></canvas>
+        </fieldset>
+    </div>
 
     <script>
     // Récupération du contexte du canvas pour le diagramme des pays oui
