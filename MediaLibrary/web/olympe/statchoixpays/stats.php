5ffa891658e209ab7e68d04d8f83f153401e09ee
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-08-29 14:43:25 +0200

    stats olympe

---
+++ MediaLibrary/web/olympe/statchoixpays/stats.php
@@ -0,0 +1,61 @@
+<?php
+require_once '../utils/auth.php';
+require_once '../utils/config.php';
+
+// Démarrage de la session
+session_start();
+
+$username = $_SESSION['username'] ?? '';
+$email = $_SESSION['email'] ?? '';
+$loggedInUser = getLoggedInUser();
+
+// Vérification si l'utilisateur est autorisé à accéder à la page
+if ($username !== "Nathan" || $email !== "nathan.gregoar@yahoo.fr") {
+    // Redirection vers la page d'accueil
+    header("Location: ../accueil/index.php");
+    exit();
+}
+
+// Connexion à la base de données (à adapter avec vos informations d'accès)
+$host = 'db';
+$dbuser = 'nathan';
+$dbpassword = '444719';
+$dbname = 'media_library';
+
+$connection = new mysqli($host, $dbuser, $dbpassword, $dbname);
+
+if ($connection->connect_error) {
+    die('Erreur de connexion : ' . $connection->connect_error);
+}
+
+// Requête SQL pour compter le nombre d'enregistrements dans la table "olympe"
+$query = "SELECT COUNT(id) AS total FROM olympe";
+$result = $connection->query($query);
+
+if ($result) {
+    $row = $result->fetch_assoc();
+    $totalGods = $row['total'];
+} else {
+    $totalGods = 0; // En cas d'erreur dans la requête
+}
+
+$connection->close();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>L'Olympe - Stats choix de destination</title>
+    <link rel="stylesheet" type="text/css" href="./stats.css">
+</head>
+<body>
+    <div class="navbar">
+        <a href="../../accueil/index.php">Accueil</a>
+        <a href="../../olympe/olympe.php">L'Olympe</a>
+        <a href="../../olympe/statchoixpays/stats.php" style="color: #D7EBF3;">Stats</a>
+        <a href="../../ecollyday/ecollyday.php">Ecollyday</a>        
+    </div>
+    <h1>Bienvenue dans l'Olympe <?php echo $username;?> - Stats choix de la destination Summer 2024</h1>
+    <h2><?php echo $totalGods; ?> Dieux de l'Olympe ont répondu au formulaire !</h2>
+</body>
+</html>
