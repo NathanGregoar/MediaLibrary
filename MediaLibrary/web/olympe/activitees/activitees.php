851b607b9d4325b8af6eced82c7458489e85a5db
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-09-25 12:39:31 +0200

    Update activitees.php

--- MediaLibrary/web/olympe/activitees/activitees.php
+++ MediaLibrary/web/olympe/activitees/activitees.php
@@ -25,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     foreach ($_POST as $key => $value) {
         if (strpos($key, 'activite_') === 0 && $value === 'on') {
             // Ajoutez la valeur de la case cochée à la liste
-            $selectedCheckboxes[] = $connection->real_escape_string($_POST[$key]);
+            $selectedCheckboxes[] = $connection->real_escape_string(substr($key, strlen('activite_')));
         }
     }
 
@@ -78,15 +78,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <div class="input-group">
                 <label>Activités souhaitées :</label>
                 <div>
-                    <input type="checkbox" id="activite_1" name="activite_1" value="Activité 1">
+                    <input type="checkbox" id="activite_1" name="activite_1" <?= isChecked("activite_1"); ?>>
                     <label for="activite_1">Activité 1</label>
                 </div>
                 <div>
-                    <input type="checkbox" id="activite_2" name="activite_2" value="Activité 2">
+                    <input type="checkbox" id="activite_2" name="activite_2" <?= isChecked("activite_2"); ?>>
                     <label for="activite_2">Activité 2</label>
                 </div>
                 <div>
-                    <input type="checkbox" id="activite_3" name="activite_3" value="Activité 3">
+                    <input type="checkbox" id="activite_3" name="activite_3" <?= isChecked("activite_3"); ?>>
                     <label for="activite_3">Activité 3</label>
                 </div>
                 <!-- Ajoutez d'autres cases à cocher pour les activités ici -->
@@ -101,3 +101,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     </form>
 </body>
 </html>
+
+<?php
+function isChecked($checkboxName) {
+    if (isset($_POST[$checkboxName]) && $_POST[$checkboxName] === 'on') {
+        return 'checked';
+    }
+    return '';
+}
+?>
