ddb46f836fb693943404502b0e986a8c78cc55df
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-09-28 17:58:57 +0200

    hb

--- MediaLibrary/web/olympe/anniversaire/anniversaire.php
+++ MediaLibrary/web/olympe/anniversaire/anniversaire.php
@@ -33,46 +33,48 @@ if ($connection->connect_error) {
         <a href="../../olympe/anniversaire/anniversaire.php" class="select">Anniversaire</a> 
     </div>
 
-    <!-- Formulaire de Date d'Anniversaire -->
-    <h2>Date d'Anniversaire</h2>
-    <form action="traitement_date_anniversaire.php" method="post">
-        <label for="date_anniversaire">Date d'anniversaire :</label>
-        <input type="date" id="date_anniversaire" name="date_anniversaire">
-        <input type="submit" value="Enregistrer">
-    </form>
+    <section class="form">
+        <!-- Formulaire de Date d'Anniversaire -->
+        <h2>Date d'Anniversaire</h2>
+        <form action="traitement_date_anniversaire.php" method="post">
+            <label for="date_anniversaire">Date d'anniversaire :</label>
+            <input type="date" id="date_anniversaire" name="date_anniversaire">
+            <input type="submit" value="Enregistrer">
+        </form>
 
-    <!-- Formulaire pour Enregistrer un Cadeau Souhaité -->
-    <h2>Enregistrer un Cadeau Souhaité</h2>
-    <form action="traitement_cadeau.php" method="post" enctype="multipart/form-data">
-        <label for="photo_cadeau">Photo de l'objet :</label>
-        <input type="file" id="photo_cadeau" name="photo_cadeau" onchange="previewImage()">
+        <!-- Formulaire pour Enregistrer un Cadeau Souhaité -->
+        <h2>Enregistrer un Cadeau Souhaité</h2>
+        <form action="traitement_cadeau.php" method="post" enctype="multipart/form-data">
+            <label for="photo_cadeau">Photo de l'objet :</label>
+            <input type="file" id="photo_cadeau" name="photo_cadeau" onchange="previewImage()">
 
-        <!-- Ajout d'une div pour la prévisualisation de l'image -->
-        <div id="imagePreview" style="display: none;">
-            <img id="preview" src="" alt="Image Preview" width="200">
-        </div>
+            <!-- Ajout d'une div pour la prévisualisation de l'image -->
+            <div id="imagePreview" style="display: none;">
+                <img id="preview" src="" alt="Image Preview" width="200">
+            </div>
 
-        <label for="description_cadeau">Description de l'objet :</label>
-        <input type="text" id="description_cadeau" name="description_cadeau">
-        
-        <label for="categorie_cadeau">Catégorie/Thème :</label>
-        <select id="categorie_cadeau" name="categorie_cadeau">
-            <option value="Mode">Mode</option>
-            <option selected value="Technologie">Technologie</option>
-            <option value="Livres">Livres</option>
-            <option value="Cuisine">Cuisine</option>
-            <option value="Beauté">Beauté</option>
-            <option value="Sports">Sports</option>
-            <option value="Art">Créativité</option>
-            <option value="Musique">Musique</option>
-            <option value="Voyages">Voyages</option>
-            <option value="Déco">Déco</option>
-            <option value="Personnalisation">Personnalisation</option>
-            <option value="Autres">Autres</option>
-        </select>
-        
-        <input type="submit" value="Enregistrer">
-    </form>
+            <label for="description_cadeau">Description de l'objet :</label>
+            <input type="text" id="description_cadeau" name="description_cadeau">
+            
+            <label for="categorie_cadeau">Catégorie/Thème :</label>
+            <select id="categorie_cadeau" name="categorie_cadeau">
+                <option value="Mode">Mode</option>
+                <option selected value="Technologie">Technologie</option>
+                <option value="Livres">Livres</option>
+                <option value="Cuisine">Cuisine</option>
+                <option value="Beauté">Beauté</option>
+                <option value="Sports">Sports</option>
+                <option value="Art">Créativité</option>
+                <option value="Musique">Musique</option>
+                <option value="Voyages">Voyages</option>
+                <option value="Déco">Déco</option>
+                <option value="Personnalisation">Personnalisation</option>
+                <option value="Autres">Autres</option>
+            </select>
+            
+            <input type="submit" value="Enregistrer">
+        </form>
+    </section>
 
     <script>
         // Fonction pour afficher la prévisualisation de l'image
