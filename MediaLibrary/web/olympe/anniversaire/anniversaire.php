eb2ee7d12e771930309a256e502f892ef9bab99e
NathanGregoar <64077468+NathanGregoar@users.noreply.github.com>
2023-09-28 17:16:35 +0200

    Update anniversaire.php

--- MediaLibrary/web/olympe/anniversaire/anniversaire.php
+++ MediaLibrary/web/olympe/anniversaire/anniversaire.php
@@ -32,5 +32,28 @@ if ($connection->connect_error) {
         <a href="../../olympe/olympe.php">L'Olympe</a> 
         <a href="../../olympe/anniversaire/anniversaire.php" class="select">Anniversaire</a> 
     </div>
+
+    <!-- Formulaire de Date d'Anniversaire -->
+    <h2>Date d'Anniversaire</h2>
+    <form action="traitement_date_anniversaire.php" method="post">
+        <label for="date_anniversaire">Date d'anniversaire :</label>
+        <input type="date" id="date_anniversaire" name="date_anniversaire">
+        <input type="submit" value="Enregistrer">
+    </form>
+
+    <!-- Formulaire pour Enregistrer un Cadeau Souhaité -->
+    <h2>Enregistrer un Cadeau Souhaité</h2>
+    <form action="traitement_cadeau.php" method="post" enctype="multipart/form-data">
+        <label for="description_cadeau">Description de l'objet :</label>
+        <input type="text" id="description_cadeau" name="description_cadeau">
+        
+        <label for="photo_cadeau">Photo de l'objet :</label>
+        <input type="file" id="photo_cadeau" name="photo_cadeau">
+        
+        <label for="categorie_cadeau">Catégorie/Thème :</label>
+        <input type="text" id="categorie_cadeau" name="categorie_cadeau">
+        
+        <input type="submit" value="Enregistrer">
+    </form>
 </body>
 </html>
